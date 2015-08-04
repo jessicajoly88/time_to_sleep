1dabccc4de1812a63c72e32c453781134fd8eada
Jake Glassman & Jessica Joly <jessicajoly88@gmail.com>
2015-08-04 14:45:25 -0700

    Final version using class method instead of defining function

--- lib/time_to_sleep.rb
+++ lib/time_to_sleep.rb
@@ -1,8 +1,10 @@
-  define_method(:time_to_sleep) do |year,month,day|
-   date = Time.new(year, month, day)
+class Time
+  define_method(:time_to_sleep) do
+   date = self
     if date.monday?() || date.tuesday?() || date.wednesday?() || date.thursday?() || date.friday?()
       "You can't sleep in"
     else
-      "You can sleep in"        
+      "You can sleep in"
     end
   end
+end
--- spec/time_to_sleep_spec.rb
+++ spec/time_to_sleep_spec.rb
@@ -3,9 +3,11 @@ require('time_to_sleep')
 
 describe('time_to_sleep') do
   it("determine if it's a weekday") do
-      expect(time_to_sleep(2015,8,4)).to(eq("You can't sleep in"))
+    date = Time.new(2015,8,4)
+    expect(date.time_to_sleep()).to(eq("You can't sleep in"))
   end
   it("determine if it's a weekend day") do
-      expect(time_to_sleep(2015,8,8)).to(eq("You can sleep in"))
+    date = Time.new(2015,8,8)
+    expect(date.time_to_sleep()).to(eq("You can sleep in"))
   end
 end
